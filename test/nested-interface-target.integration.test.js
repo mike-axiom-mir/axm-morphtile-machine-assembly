07d1a05ab9a853cafce0a03cfdf1c798cd0ef268
mike-axiom-mir <miketobi90@gmail.com>
2026-09-20 15:26:55 +0200

    Re-prove nested Interface target boundary on merged MorphTile core

--- test/nested-interface-target.integration.test.js
+++ test/nested-interface-target.integration.test.js
@@ -11,7 +11,7 @@ const interfaceCommit = process.env.INTERFACE_NESTED_COMMIT;
 const corePath = process.env.MORPHTILE_CORE_PATH;
 const coreCommit = process.env.MORPHTILE_COMMIT;
 const EXPECTED_INTERFACE_COMMIT = "3c715d8aa0138472000472d584f990a88a543ecf";
-const EXPECTED_MORPHTILE_COMMIT = "26b89a77f6a90715a6742dc4d084008ba63731b6";
+const EXPECTED_MORPHTILE_COMMIT = "d2d2df0e4ad88f1cda885e3eb1394151515e7946";
 const ready = Boolean(interfacePath && corePath);
 
 function req(id, goal, intent) {

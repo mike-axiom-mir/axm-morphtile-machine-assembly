ca1fdec657b697def78e6930750169bbc0bc270c
mike-axiom-mir <miketobi90@gmail.com>
2026-09-20 22:50:40 +0200

    test: bind nested Interface proof to merged heads [skip ci]

--- test/nested-interface-target.integration.test.js
+++ test/nested-interface-target.integration.test.js
@@ -10,8 +10,8 @@ const interfacePath = process.env.INTERFACE_NESTED_MACHINE_PATH;
 const interfaceCommit = process.env.INTERFACE_NESTED_COMMIT;
 const corePath = process.env.MORPHTILE_CORE_PATH;
 const coreCommit = process.env.MORPHTILE_COMMIT;
-const EXPECTED_INTERFACE_COMMIT = "0e47cd09d613c6c934cd7adbdb6c7a18eeaaf6ed";
-const EXPECTED_MORPHTILE_COMMIT = "43a9eccf831a49aedc169a5ce0b6701e22ae4cc3";
+const EXPECTED_INTERFACE_COMMIT = "271fbf2abe6fc214d30076b9417655f084221eac";
+const EXPECTED_MORPHTILE_COMMIT = "2bdf8eade1376055473b9cc1b11734b72a5566e5";
 const ready = Boolean(interfacePath && corePath);
 
 function req(id, goal, intent) {
